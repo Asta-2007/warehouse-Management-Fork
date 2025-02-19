08768c15ab13fdf01e521fa1caced0b034001026
Asta-2007 <docker624@gmail.com>
2025-02-19 12:39:56 +0700

    update logic for auth

--- lib/page/app.dart
+++ lib/page/app.dart
@@ -5,35 +5,40 @@ import 'package:werehouse_inventory/page/middle_page.dart';
 import 'package:werehouse_inventory/shered_data_to_root/websocket_helper.dart';
 import 'package:werehouse_inventory/theme_data/theme_page.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
   Widget build(BuildContext context) {
+    final secondaryWs = Provider.of<WebsocketHelper>(context, listen: true);
+    secondaryWs.chekVerifikasi();
+    secondaryWs.removeTokenIfExp();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: ThemeColorPage.lightTheme,
       darkTheme: ThemeColorPage.darkTheme,
-      home: Consumer<WebsocketHelper>(
-        builder: (context, wsHelper, child) {
+      home: StreamBuilder(
+        stream: secondaryWs.verifikasiLogin(),
+        builder: (context, snapshot) {
+          // print('${snapshot.data} status');
           /*
-          check token user in local storage
-          */
-          return StreamBuilder(
-            stream: wsHelper.verifikasi(),
-            builder: (context, snapshot) {
-              // print('${snapshot.data} status');
-              /*
                check if token user is valide else is token invalide and nothing return
               */
-
-              if (snapshot.hasData) {
-                return HomePage();
-              }
-              return MiddlePage();
-            },
-          );
+          print("has data ${snapshot.hasData}");
+          if (snapshot.data == null) {
+            return MiddlePage();
+          }
+          if (snapshot.data != null) {
+            return HomePage();
+          }
+          return MiddlePage();
         },
       ),
     );
